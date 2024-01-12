11a912748fef0a460ee701147cc8d8a3f0ae2656
EyesIce <matteo.gabrieli1996@gmail.com>
2024-01-12 10:15:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     const daysBox = document.getElementById('dayMonth');
     const calculateButton = document.getElementById('calculate');
-    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     const dayDuration = document.getElementById('period-duration');
 
     let selectedDate = new Date();
-    
-    function createYearGrid() {
-        const yearGrid = document.querySelector('.main-months');
-//        yearGrid.innerHTML = renderMonths();
-    
-        const selectableMonths = yearGrid.querySelectorAll('.selectable-month');
-        selectableMonths.forEach((month, index) => {
-            month.addEventListener('click', function () {
-                const selectedMonth = index;
-                selectedDate.setMonth(selectedMonth);
-                updateMonthHeader();
-                createDayGrid(selectedDate.getFullYear(), selectedDate.getMonth());
-            });
-        });
-   
-        // Inizializza l'header dell'anno
-        updateYearHeader();
-    }
     
     function updateYearHeader() {
         const yearValue = document.querySelector('.year-value');
@@ -32,35 +14,48 @@ document.addEventListener('DOMContentLoaded', function () {
     function prevYear() {
         selectedDate.setFullYear(selectedDate.getFullYear() - 1);
         updateYearHeader();
-//        createYearGrid();
+        updateMonthHeader();
         createDayGrid(selectedDate.getFullYear(), selectedDate.getMonth());
     }    
 
     function nextYear() {
         selectedDate.setFullYear(selectedDate.getFullYear() + 1);
         updateYearHeader();
-//        createYearGrid();
+        updateMonthHeader();
         createDayGrid(selectedDate.getFullYear(), selectedDate.getMonth());
     }
-
-
+    
     function createMonthGrid(month, year) {
         const monthGrid = document.querySelector('.main-months');
     
         const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     
-  
         // Itera attraverso i mesi e crea un div per ognuno
         for (let i = 0; i < months.length; i++) {
-          const monthDiv = document.createElement("div");
-          monthDiv.className = "month selectable-month";
-          monthDiv.textContent = months[i];
-          monthGrid.appendChild(monthDiv);
+            const monthDiv = document.createElement("div");
+    
+            if (i === selectedDate.getMonth()) {
+                monthDiv.className = "month selectable-month selected-month";
+            } else {
+                monthDiv.className = "month selectable-month";
+            }
+    
+            monthDiv.textContent = months[i];
+            monthGrid.appendChild(monthDiv);
         }
     
         const selectableMonths = monthGrid.querySelectorAll('.selectable-month');
         selectableMonths.forEach((month, index) => {
             month.addEventListener('click', function () {
+                // Remove the 'selected-month' class from all selectable months
+                const selectedMonthDiv = monthGrid.querySelector('.selected-month');
+                if (selectedMonthDiv) {
+                    selectedMonthDiv.classList.remove('selected-month');
+                }
+    
+                // Add the 'selected-month' class to the clicked month
+                month.classList.add('selected-month');
+    
                 const selectedMonth = index;
                 selectedDate.setMonth(selectedMonth);
                 updateMonthHeader();
@@ -68,18 +63,21 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     }
+    
 
     function prevMonth() {
         selectedDate.setMonth(selectedDate.getMonth() - 1);
         updateMonthHeader();
-//        createYearGrid();
+        updateYearHeader();
+
         createDayGrid(selectedDate.getFullYear(), selectedDate.getMonth());
     }    
 
     function nextMonth() {
         selectedDate.setMonth(selectedDate.getMonth() + 1);
         updateMonthHeader();
-//        createYearGrid();
+        updateYearHeader();
+
         createDayGrid(selectedDate.getFullYear(), selectedDate.getMonth());
     }
 
@@ -87,6 +85,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const monthGrid = document.querySelector('.main-days');
         monthGrid.innerHTML = ''; // Svuota la griglia dei giorni
     
+        const currentDate = new Date();
         const firstDay = new Date(year, month, 1);
         const lastDay = new Date(year, month + 1, 0);
         const daysInMonth = lastDay.getDate();
@@ -113,20 +112,36 @@ document.addEventListener('DOMContentLoaded', function () {
             dayElement.textContent = i;
             dayElement.classList.add('selectable-day');
             dayElement.addEventListener('click', function () {
-                selectDate(new Date(year, month, i));
+                dayClicked(year, month, i);
             });
             monthGrid.appendChild(dayElement);
         }
-    }
     
-    function selectDate(date) {
-        selectedDate = date;
+        // Highlight the current day if no date has been clicked
+        if (!selectedDate) {
+            const currentDay = currentDate.getDate();
+            const todayElement = monthGrid.querySelector('.selectable-day:nth-child(' + (currentDay + startingDay) + ')');
+            if (todayElement) {
+                todayElement.classList.add('current-day');
+            }
+        }
+    }
+
+    function updateMonthHeader() {
+        // Aggiorna l'header con il nome del mese selezionato
+        daysBox.querySelector('header span').innerHTML = months[selectedDate.getMonth()] + ' ' + selectedDate.getFullYear();
+    }
+
+
+    function selectDate(clickedDate) {
+        selectedDate = clickedDate; // Use the provided date or default to the current date
         createDayGrid(selectedDate.getFullYear(), selectedDate.getMonth());
     }
     
-    function updateMonthHeader() {
-        // Aggiorna l'header con il nome del mese selezionato
-        daysBox.querySelector('header span').innerHTML = months[selectedDate.getMonth()];
+    // Example of how to use selectDate when a day is clicked
+    function dayClicked(year, month, day) {
+        const clickedDate = new Date(year, month, day);
+        selectDate(clickedDate);
     }
     
     function selectDuration(){
@@ -138,36 +153,33 @@ document.addEventListener('DOMContentLoaded', function () {
         dayDuration.value = 28;
     }
 
-/*    function updateDuration() {
-        const durationInput = document.getElementById('durationInput'); // Assumendo che 'durationInput' sia l'id del tuo campo di input
-        const date = new Date(); // Supponiamo che tu voglia ottenere la data corrente
-
-        durationInput.value = 28; // Imposta il valore predefinito a 28
-
-        durationInput.addEventListener('click', function () {
-            const duration = parseInt(durationInput.value) || 28;
-            const datePeriod = date.getDay() + duration; // Nota la correzione di date.getDay() per ottenere il giorno corrente
-            console.log(datePeriod);
-        });
-
+    function periodCalculate(selectedDate, duration) {
+        const ovulationDay = new Date(selectedDate.getTime() + parseInt(duration) * 24 * 60 * 60 * 1000);
+        console.log('Ovulation Date:', ovulationDay);
     }
-*/
+     
+
     function init() {
         const currentYear = selectedDate.getFullYear();
         const currentMonth = selectedDate.getMonth();
-        
-        createYearGrid();
+        const currentDay = selectedDate.getDay();
+        updateYearHeader();
         createMonthGrid(currentMonth, currentYear);
         updateMonthHeader();
         createDayGrid(selectedDate.getFullYear(), selectedDate.getMonth());
         
+        dayClicked(currentYear,currentMonth,currentDay);
+
         document.getElementById('prevYear').addEventListener('click', prevYear);
         document.getElementById('nextYear').addEventListener('click', nextYear);
         document.getElementById('prevMonth').addEventListener('click', prevMonth);
         document.getElementById('nextMonth').addEventListener('click', nextMonth);
 
         selectDuration();
-//        calculateButton.addEventListener('click', updateDuration);
+
+        document.getElementById('calculate').addEventListener('click', function () {
+            periodCalculate(selectedDate, dayDuration.value);
+        });
 
     }
 
